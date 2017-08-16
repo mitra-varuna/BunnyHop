ecc4f0e02ebc1734abdc217814df01f13b9140dc
Sushant <sushant.srivastav@gmail.com>
2017-08-16 14:07:50 +0530

    First cut

---
+++ android/src/com/podcastmentat/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.podcastmentat.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.podcastmentat.game.BunnyHopGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BunnyHopGame(), config);
+	}
+}
